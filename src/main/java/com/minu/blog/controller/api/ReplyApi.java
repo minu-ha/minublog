4138611e17251fb195f4f057e9f42d2133503cdd
wumeenna <minu0604@naver.com>
2022-07-16 23:26:30 +0900

    2022-07-16

---
+++ src/main/java/com/minu/blog/controller/api/ReplyApi.java
@@ -0,0 +1,11 @@
+package com.minu.blog.controller.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ReplyApi{
+
+
+}
